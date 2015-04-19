77ac109960c9b0f775f00a0567d76dbff346ce84
Maarten Billemont <lhunath@lyndir.com>
2015-04-19 15:05:30 -0400

    Use iscroll instead of stellar for a smoother scroll.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 (function ($, window) {
-    $.stellar();
+    myScroll = new IScroll('#iscroll', {
+        mouseWheel: true,
+        indicators: [{
+            el: $('.iscroll.slow3')[0],
+            resize: false,
+            ignoreBoundaries: true,
+            speedRatioY: -0.3
+        }, {
+            el: $('.iscroll.slow4')[0],
+            resize: false,
+            ignoreBoundaries: true,
+            speedRatioY: -0.4
+        }]
+    });
 }(jQuery, window));
